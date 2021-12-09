261a3e036ca6da82ab0101deacf36d6526e1876e
JenhaniChedli <84502320+JenhaniChedli@users.noreply.github.com>
2021-12-09 10:34:14 +0100

    add bootstrap / create table Ticket

---
+++ database/migrations/2021_12_09_093143_create_tickets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('nomEvent');
+            $table->date('dateEvent');
+            $table->longText('description');
+            $table->string('photo');
+            $table->integer('nbrTicket');
+            $table->float('prix', 8, 3);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
